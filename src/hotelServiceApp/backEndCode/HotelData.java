633b4ec705d5102b16cfb706e6b9800651bf80d9
Gazaba Ávila Leonardo Damián <locusaa1@gmail.com>
2021-05-30 21:52:18 -0300

    Comments and User Main menu
    Added comments for every method in the project.
    Finished the code of the user main menu.
    Still remaining the Amind an Recepcionist menus and improve the graphics of all the interfaces.

--- src/hotelServiceApp/backEndCode/HotelData.java
+++ src/hotelServiceApp/backEndCode/HotelData.java
@@ -1,7 +1,7 @@
 package hotelServiceApp.backEndCode;
 
-import com.sun.deploy.panel.ExceptionListDialog;
-import com.sun.org.apache.xpath.internal.operations.Bool;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -12,12 +12,14 @@ public class HotelData {
     private List<Passenger> passengerList;
     private List<Room> roomList;
     private List<Booking> bookingList;
+    private List<SupplyItem> supplyItemList;
 
     public HotelData() {
 
         this.passengerList = new ArrayList<>();
         this.roomList = new ArrayList<>();
         this.bookingList = new ArrayList<>();
+        this.supplyItemList = new ArrayList<>();
     }
 
     public List<Passenger> getPassengerList() {
@@ -36,26 +38,39 @@ public class HotelData {
     }
 
     public List<Room> getRoomList() {
+
         return roomList;
     }
 
     public void setRoomList(Room room) {
+
         this.roomList.add(room);
     }
 
     public List<Booking> getBookingList() {
+
         return bookingList;
     }
 
     public void setBookingList(Booking booking) {
+
         this.bookingList.add(booking);
+    }
+
+    public List<SupplyItem> getSupplyItemList() {
+
+        return supplyItemList;
+    }
+
+    public void setSupplyItemList(SupplyItem supplyItem) {
+
+        this.supplyItemList.add(supplyItem);
     }
 
     /**
      * public Boolean passengerExists(String dni)
      * This method searches in the passengerList of the HotelData if the dni in the parameter is already saved on a existing passenger.
      */
-
     public Boolean passengerExists(String dni) {
 
         Boolean confirm = false;
@@ -71,7 +86,6 @@ public class HotelData {
      * public Boolean usernameExists(String username)
      * This method searches in the PassengerList of the HotelData if the username is already taken.
      */
-
     public Boolean usernameExists(String username) {
 
         Boolean confirm = false;
@@ -88,7 +102,6 @@ public class HotelData {
      * This method searches in the PassengerList of the HotelData and return the passenger.
      * We assume that the passenger already exists. It needs to be checked first using the method passengerExists.
      */
-
     public Passenger usernameSearchPassenger(String username) {
 
         Passenger passenger = null;
@@ -104,7 +117,6 @@ public class HotelData {
      * public Boolean confirmPassword(String password, Passenger passenger)
      * This method confirms if the password parameter matches the passenger parameter password
      */
-
     public Boolean confirmPassword(String password, Passenger passenger) {
 
         Boolean confirm = false;
@@ -114,6 +126,10 @@ public class HotelData {
         return confirm;
     }
 
+    /**
+     * public Room searchAvailableRoom(LocalDate firstDate, LocalDate secondDate, int amountOfRooms)
+     * This method confirms if there is an available room within the two localDates in parameters and with the amount of rooms specified.
+     */
     public Room searchAvailableRoom(LocalDate firstDate, LocalDate secondDate, int amountOfRooms) {
 
         Room roomFound = null;
@@ -127,6 +143,10 @@ public class HotelData {
         return roomFound;
     }
 
+    /**
+     * public void setNewBooking(Passenger passenger, Room room, Booking booking)
+     * This method sets a new booking into the specific room, into the bookingList and into the passenger.
+     */
     public void setNewBooking(Passenger passenger, Room room, Booking booking) {
 
         passenger.setBooking(booking);
@@ -134,22 +154,64 @@ public class HotelData {
         this.setBookingList(booking);
     }
 
-    public void deleteBooking(Booking booking){
+    /**
+     * public void deleteBooking(Booking booking)
+     * This method deletes an existing booking searching if it is inside the list.
+     * It could not be deleted inside the iteration because it will throw an Exception.
+     */
+    public void deleteBooking(Booking booking) {
 
-        int index=0;
-        for (Booking bookingFound : this.bookingList){
+        Boolean confirm = false;
+        for (Booking bookingFound : this.bookingList) {
 
-            if (bookingFound.equals(booking)){
-                this.bookingList.set(index,null);
+            if (bookingFound.equals(booking)) {
+
+                confirm = true;
             }
-            index++;
+        }
+        if (confirm) {
+
+            this.bookingList.remove(booking);
         }
     }
 
-    public void deleteExistingBooking(Passenger passenger, Room room, Booking booking){
+    /**
+     * public void deleteExistingBooking(Passenger passenger, Room room, Booking booking)
+     * This method deletes an existing book from the passenger, this list and from the room.
+     */
+    public void deleteExistingBooking(Passenger passenger, Room room, Booking booking) {
 
         passenger.setBooking(null);
         room.deleteBooking(booking);
         this.deleteBooking(booking);
+    }
+
+    /**
+     * public ObservableList<SupplyItem> getItemsInStock(SupplySections section)
+     * This method searches into the hotel list if there is stock of the items with the section passed by parameter.
+     * It add the items into an observable list to return it and be used in a ListView
+     */
+    public ObservableList<SupplyItem> getItemsInStock(SupplySections section) {
+
+        List<SupplyItem> items = new ArrayList<>();
+
+        for (SupplyItem item : this.supplyItemList) {
+
+            if (item.getSection().equals(section) && item.getStock() > 0) {
+
+                items.add(item);
+            }
+        }
+        ObservableList<SupplyItem> itemsInStock = FXCollections.observableList(items);
+        return itemsInStock;
+    }
+
+    /**
+     * public void setNewPurchase(Passenger passenger, Purchase purchase)
+     * This method adds a new purchase to the passenger in the parameter.
+     */
+    public void setNewPurchase(Passenger passenger, Purchase purchase) {
+
+        passenger.setPurchases(purchase);
     }
 }
